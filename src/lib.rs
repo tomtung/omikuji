c42370977a89fefe12b4b60929a23e8a718bdbde
Yubing Dong (Tom) <tom.tung.dyb@gmail.com>
2018-11-24 20:23:31 -0800

    Implement balanced 2-means

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,14 @@ extern crate log;
 
 extern crate time;
 
+extern crate rand;
+
+extern crate order_stat;
+
+#[cfg(test)]
+#[macro_use]
+extern crate assert_approx_eq;
+
 pub mod data;
 
 pub mod model;
