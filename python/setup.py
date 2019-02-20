a807453f072511033b54a686046de4908d6d9800
Yubing Dong (Tom) <tom.tung.dyb@gmail.com>
2019-02-20 00:37:21 -0800

    Add more metadata to setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 import traceback
+import os
 
 
 class OpenBlasConfig:
@@ -51,15 +52,46 @@ def show_message(*lines):
     print("=" * columns)
 
 
+with open(
+    os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), os.path.pardir, "README.md"
+    ),
+    encoding="utf-8",
+) as f:
+    LONG_DESCRIPTION = "\n" + f.read()
+
+
 def run_setup(with_openblas):
     setup(
         name="parabel",
         version="0.0.1",
+        description=(
+            "Python binding to Parabel-rs, a highly parallelized 🦀Rust implementation of Parabel "
+            "for extreme multi-label classification"
+        ),
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        python_requires=">=3.6.0",
+        url="https://github.com/tomtung/parabel-rs",
+        license="MIT",
         packages=["parabel"],
         zip_safe=False,
         platforms="any",
         setup_requires=["milksnake"],
         milksnake_tasks=[make_milksnake_task(with_openblas)],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Programming Language :: Rust",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+            "Topic :: Software Development :: Libraries",
+        ],
     )
 
 
