3c19d17cbed4f1d8e38352fc2345a7c0399bc38c
Yubing Dong (Tom) <tom.tung.dyb@gmail.com>
2019-02-19 21:46:56 -0800

    Make Python binding attempt building with OpenBLAS first, and fall back to not using OpenBLAS upon failure

--- python/setup.py
+++ python/setup.py
@@ -1,37 +1,78 @@
 from setuptools import setup
-import sys
-
-# Add option to enable OpenBLAS; a bit hacky but works
-if "--with-openblas" in sys.argv:
-    with_openblas = True
-    sys.argv.remove("--with-openblas")
-else:
-    with_openblas = False
+import traceback
 
 
-def build_native(spec):
-    cmd = ["cargo", "build", "--release"]
-    if with_openblas:
-        cmd.extend(["--features", "openblas"])
+class OpenBlasConfig:
+    NONE = 0
+    STATIC = 1
+    SYSTEM = 2
 
-    build = spec.add_external_build(cmd=cmd, path="../c-api")
 
-    spec.add_cffi_module(
-        module_path="parabel._libparabel",
-        dylib=lambda: build.find_dylib("parabel", in_path="../c-api/target/release"),
-        header_filename=lambda: build.find_header(
-            "parabel.h", in_path="../c-api/target/include"
-        ),
-        rtld_flags=["NOW", "NODELETE"],
+def make_milksnake_task(openblas_config):
+    def build(spec):
+        cmd = ["cargo", "build", "--release"]
+        if openblas_config == OpenBlasConfig.NONE:
+            pass
+        elif openblas_config == OpenBlasConfig.STATIC:
+            cmd.extend(["--features", "openblas-static"])
+        elif openblas_config == OpenBlasConfig.SYSTEM:
+            cmd.extend(["--features", "openblas"])
+        else:
+            assert False, "Unkown OpenBLAS Config: " + repr(openblas_config)
+
+        build = spec.add_external_build(cmd=cmd, path="../c-api")
+
+        spec.add_cffi_module(
+            module_path="parabel._libparabel",
+            dylib=lambda: build.find_dylib(
+                "parabel", in_path="../c-api/target/release"
+            ),
+            header_filename=lambda: build.find_header(
+                "parabel.h", in_path="../c-api/target/include"
+            ),
+            rtld_flags=["NOW", "NODELETE"],
+        )
+
+    return build
+
+
+def show_message(*lines):
+    try:
+        import shutil
+
+        columns = shutil.get_terminal_size((80, 20)).columns
+    except ImportError:
+        columns = 80
+
+    print("=" * columns)
+    for line in lines:
+        print(line)
+
+    print("=" * columns)
+
+
+def run_setup(with_openblas):
+    setup(
+        name="parabel",
+        version="0.0.1",
+        packages=["parabel"],
+        zip_safe=False,
+        platforms="any",
+        setup_requires=["milksnake"],
+        milksnake_tasks=[make_milksnake_task(with_openblas)],
     )
 
 
-setup(
-    name="parabel",
-    version="0.0.1",
-    packages=["parabel"],
-    zip_safe=False,
-    platforms="any",
-    setup_requires=["milksnake"],
-    milksnake_tasks=[build_native],
-)
+try:
+    run_setup(OpenBlasConfig.SYSTEM)
+except:
+    traceback.print_exc()
+    show_message("Failed to build with system OpenBLAS; try compiling bundled OpenBLAS")
+    try:
+        run_setup(OpenBlasConfig.STATIC)
+    except:
+        show_message("Failed to build with OpenBLAS; try building without OpenBLAS")
+        run_setup(OpenBlasConfig.NONE)
+        show_message(
+            "Successfully built but without OpenBLAS; prediction speed will be affected"
+        )
