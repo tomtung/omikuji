be5022c27a46035e9320e02c692fa9ec14d10b06
Yubing Dong (Tom) <tom.tung.dyb@gmail.com>
2019-02-24 01:15:27 -0800

    Remove optional dependency on OpenBLAS
    Because we don't rely on fast dense-dense multiplication anymore.

--- python/setup.py
+++ python/setup.py
@@ -1,55 +1,19 @@
 from setuptools import setup
-import traceback
 import os
 
 
-class OpenBlasConfig:
-    NONE = 0
-    STATIC = 1
-    SYSTEM = 2
-
-
-def make_milksnake_task(openblas_config):
-    def build(spec):
-        cmd = ["cargo", "build", "--release"]
-        if openblas_config == OpenBlasConfig.NONE:
-            pass
-        elif openblas_config == OpenBlasConfig.STATIC:
-            cmd.extend(["--features", "openblas-static"])
-        elif openblas_config == OpenBlasConfig.SYSTEM:
-            cmd.extend(["--features", "openblas"])
-        else:
-            assert False, "Unkown OpenBLAS Config: " + repr(openblas_config)
-
-        build = spec.add_external_build(cmd=cmd, path="../c-api")
-
-        spec.add_cffi_module(
-            module_path="parabel._libparabel",
-            dylib=lambda: build.find_dylib(
-                "parabel", in_path="../c-api/target/release"
-            ),
-            header_filename=lambda: build.find_header(
-                "parabel.h", in_path="../c-api/target/include"
-            ),
-            rtld_flags=["NOW", "NODELETE"],
-        )
-
-    return build
-
-
-def show_message(*lines):
-    try:
-        import shutil
-
-        columns = shutil.get_terminal_size((80, 20)).columns
-    except ImportError:
-        columns = 80
-
-    print("=" * columns)
-    for line in lines:
-        print(line)
-
-    print("=" * columns)
+def build_native(spec):
+    build = spec.add_external_build(
+        cmd=["cargo", "build", "--release"], path="../c-api"
+    )
+    spec.add_cffi_module(
+        module_path="parabel._libparabel",
+        dylib=lambda: build.find_dylib("parabel", in_path="../c-api/target/release"),
+        header_filename=lambda: build.find_header(
+            "parabel.h", in_path="../c-api/target/include"
+        ),
+        rtld_flags=["NOW", "NODELETE"],
+    )
 
 
 with open(
@@ -61,50 +25,34 @@ with open(
     LONG_DESCRIPTION = "\n" + f.read()
 
 
-def run_setup(with_openblas):
-    setup(
-        name="parabel",
-        version="0.0.1",
-        description=(
-            "Python binding to Parabel-rs, a highly parallelized 🦀Rust implementation of Parabel "
-            "for extreme multi-label classification"
-        ),
-        long_description=LONG_DESCRIPTION,
-        long_description_content_type="text/markdown",
-        python_requires=">=3.6.0",
-        url="https://github.com/tomtung/parabel-rs",
-        license="MIT",
-        packages=["parabel"],
-        zip_safe=False,
-        platforms="any",
-        setup_requires=["milksnake"],
-        milksnake_tasks=[make_milksnake_task(with_openblas)],
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: Implementation :: CPython",
-            "Programming Language :: Rust",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Intended Audience :: Developers",
-            "Intended Audience :: Science/Research",
-            "Topic :: Scientific/Engineering :: Artificial Intelligence",
-            "Topic :: Software Development :: Libraries",
-        ],
-    )
-
-
-try:
-    run_setup(OpenBlasConfig.SYSTEM)
-except:
-    traceback.print_exc()
-    show_message("Failed to build with system OpenBLAS; try compiling bundled OpenBLAS")
-    try:
-        run_setup(OpenBlasConfig.STATIC)
-    except:
-        show_message("Failed to build with OpenBLAS; try building without OpenBLAS")
-        run_setup(OpenBlasConfig.NONE)
-        show_message(
-            "Successfully built but without OpenBLAS; prediction speed will be affected"
-        )
+setup(
+    name="parabel",
+    version="0.0.1",
+    description=(
+        "Python binding to Parabel-rs, a highly parallelized 🦀Rust implementation of Parabel "
+        "for extreme multi-label classification"
+    ),
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6.0",
+    url="https://github.com/tomtung/parabel-rs",
+    license="MIT",
+    packages=["parabel"],
+    zip_safe=False,
+    platforms="any",
+    setup_requires=["milksnake"],
+    milksnake_tasks=[build_native],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Rust",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
