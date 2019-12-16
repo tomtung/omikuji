30d00e6b7da27effcc19939dd1cdcefb2968845b
Osma Suominen <osma.suominen@helsinki.fi>
2019-12-16 13:02:32 +0200

    Add Trove classifiers for Python 3.5 and 3.8

--- setup.py
+++ setup.py
@@ -44,8 +44,10 @@ setup(
     milksnake_universal=False,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Rust",
         "License :: OSI Approved :: MIT License",
