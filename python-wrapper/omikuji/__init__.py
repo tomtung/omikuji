d79f919e798383c834711dc3667e45ba16a8df5e
Yubing Dong (Tom) <tom.tung.dyb@gmail.com>
2020-01-16 03:11:02 -0800

    Bump version number

--- python-wrapper/omikuji/__init__.py
+++ python-wrapper/omikuji/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = ["Model", "LossType"]
 
 from ._libomikuji import lib, ffi
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def load_readme():
 
 setup(
     name="omikuji",
-    version="0.2.0",
+    version="0.3.0",
     author="Tom Dong",
     author_email="tom.tung.dyb@gmail.com",
     description=(
