tomtung/omikuji

commit 074dbfd95fab1f22dfd06c6710e11cdd79bdfb4d

Fix clippy warnings

src/bin/omikuji.rs
@@ -3,9 +3,9 @@ extern crate omikuji;
 extern crate rayon;
 
 use clap::value_t;
+use simple_logger::SimpleLogger;
 use std::fs::File;
 use std::io::{BufWriter, Write};
-use simple_logger::SimpleLogger;
 
 fn set_num_threads(matches: &clap::ArgMatches) {
     rayon::ThreadPoolBuilder::new()
@@ -16,33 +16,16 @@ fn set_num_threads(matches: &clap::ArgMatches) {
 }
 
 fn parse_train_hyper_param(matches: &clap::ArgMatches) -> omikuji::model::TrainHyperParam {
-    let mut hyper_param = omikuji::model::train::HyperParam::default();
-
-    hyper_param.n_trees = value_t!(matches, "n_trees", usize).unwrap();
-    hyper_param.min_branch_size = value_t!(matches, "min_branch_size", usize).unwrap();
-    hyper_param.max_depth = value_t!(matches, "max_depth", usize).unwrap();
-    hyper_param.centroid_threshold = value_t!(matches, "centroid_threshold", f32).unwrap();
-    hyper_param.collapse_every_n_layers =
-        value_t!(matches, "collapse_every_n_layers", usize).unwrap();
-    hyper_param.tree_structure_only = matches.occurrences_of("tree_structure_only") > 0;
-    hyper_param.train_trees_1_by_1 = matches.occurrences_of("train_trees_1_by_1") > 0;
-
-    hyper_param.linear.loss_type = match matches.value_of("linear.loss").unwrap() {
-        "hinge" => omikuji::model::liblinear::LossType::Hinge,
-        "log" => omikuji::model::liblinear::LossType::Log,
-        _ => unreachable!(),
+    let hyper_param = omikuji::model::train::HyperParam {
+        n_trees: value_t!(matches, "n_trees", usize).unwrap(),
+        min_branch_size: value_t!(matches, "min_branch_size", usize).unwrap(),
+        max_depth: value_t!(matches, "max_depth", usize).unwrap(),
+        centroid_threshold: value_t!(matches, "centroid_threshold", f32).unwrap(),
+        collapse_every_n_layers: value_t!(matches, "collapse_every_n_layers", usize).unwrap(),
+        tree_structure_only: matches.is_present("tree_structure_only"),
+        train_trees_1_by_1: matches.is_present("train_trees_1_by_1"),
+        ..Default::default()
     };
-    hyper_param.linear.eps = value_t!(matches, "linear.eps", f32).unwrap();
-    hyper_param.linear.c = value_t!(matches, "linear.c", f32).unwrap();
-    hyper_param.linear.weight_threshold =
-        value_t!(matches, "linear.weight_threshold", f32).unwrap();
-    hyper_param.linear.max_iter = value_t!(matches, "linear.max_iter", u32).unwrap();
-
-    hyper_param.cluster.k = value_t!(matches, "cluster.k", usize).unwrap();
-    hyper_param.cluster.balanced = matches.occurrences_of("cluster.unbalanced") == 0;
-    hyper_param.cluster.eps = value_t!(matches, "cluster.eps", f32).unwrap();
-    hyper_param.cluster.min_size = value_t!(matches, "cluster.min_size", usize).unwrap();
-
     hyper_param.validate().unwrap();
     hyper_param
 }

src/data.rs
@@ -107,23 +107,14 @@ impl DataSet {
                 ));
             }
 
-            let n_examples = tokens[0].parse::<usize>().or_else(|_| {
-                Err(Error::new(
-                    ErrorKind::InvalidData,
-                    "Failed to parse number of examples",
-                ))
+            let n_examples = tokens[0].parse::<usize>().map_err(|_| {
+                Error::new(ErrorKind::InvalidData, "Failed to parse number of examples")
             })?;
-            let n_features = tokens[1].parse::<usize>().or_else(|_| {
-                Err(Error::new(
-                    ErrorKind::InvalidData,
-                    "Failed to parse number of features",
-                ))
+            let n_features = tokens[1].parse::<usize>().map_err(|_| {
+                Error::new(ErrorKind::InvalidData, "Failed to parse number of features")
             })?;
-            let n_labels = tokens[2].parse::<usize>().or_else(|_| {
-                Err(Error::new(
-                    ErrorKind::InvalidData,
-                    "Failed to parse number of labels",
-                ))
+            let n_labels = tokens[2].parse::<usize>().map_err(|_| {
+                Error::new(ErrorKind::InvalidData, "Failed to parse number of labels")
             })?;
 
             (n_examples, n_features, n_labels)

src/model/cluster.rs
@@ -7,7 +7,7 @@ use ordered_float::NotNan;
 use rand::prelude::*;
 use serde::{Deserialize, Serialize};
 use sprs::prod::csr_mulacc_dense_colmaj;
-use sprs::{CsMatViewI, SpIndex, MulAcc};
+use sprs::{CsMatViewI, MulAcc, SpIndex};
 use std::cmp::Reverse;
 use std::fmt::Display;
 use std::iter::Sum;

src/model/liblinear.rs
@@ -4,7 +4,6 @@ use rand::prelude::*;
 use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::f32::{INFINITY, NEG_INFINITY};
-use std::iter::FromIterator;
 use std::ops::Deref;
 
 /// The loss function used by liblinear model.
@@ -147,17 +146,20 @@ pub(crate) fn predict(
     loss_type: LossType,
     feature_vec: &SparseVec,
 ) -> DenseVec {
-    DenseVec::from_iter(weights.iter().map(|w| {
-        if let Some(w) = w {
-            let score = w.dot(feature_vec);
-            match loss_type {
-                LossType::Log => -(-score).exp().ln_1p(),
-                LossType::Hinge => -(1. - score).max(0.).powi(2),
+    weights
+        .iter()
+        .map(|w| {
+            if let Some(w) = w {
+                let score = w.dot(feature_vec);
+                match loss_type {
+                    LossType::Log => -(-score).exp().ln_1p(),
+                    LossType::Hinge => -(1. - score).max(0.).powi(2),
+                }
+            } else {
+                0.
             }
-        } else {
-            0.
-        }
-    }))
+        })
+        .collect()
 }
 
 /// A coordinate descent solver for L2-loss SVM dual problems.
@@ -172,13 +174,13 @@ pub(crate) fn predict(
 ///  D is a diagonal matrix
 ///
 /// In L1-SVM case (omitted):
-/// 		upper_bound_i = Cp if y_i = 1
-/// 		upper_bound_i = Cn if y_i = -1
-/// 		D_ii = 0
+///         upper_bound_i = Cp if y_i = 1
+///         upper_bound_i = Cn if y_i = -1
+///         D_ii = 0
 /// In L2-SVM case:
-/// 		upper_bound_i = INF
-/// 		D_ii = 1/(2*Cp)	if y_i = 1
-/// 		D_ii = 1/(2*Cn)	if y_i = -1
+///         upper_bound_i = INF
+///         D_ii = 1/(2*Cp)    if y_i = 1
+///         D_ii = 1/(2*Cn)    if y_i = -1
 ///
 /// Given:
 /// x, y, Cp, Cn

src/model/mod.rs
@@ -214,7 +214,7 @@ impl Model {
             "Model loaded; it took {:.2}s",
             start_t.elapsed().as_secs_f32()
         );
-        Ok(Self { settings, trees })
+        Ok(Self { trees, settings })
     }
 
     /// Densify model weights to speed up prediction at the cost of more memory usage.
@@ -271,20 +271,14 @@ impl TreeNode {
     }
 
     fn is_leaf(&self) -> bool {
-        if let TreeNode::Leaf { .. } = self {
-            true
-        } else {
-            false
-        }
+        matches!(self, TreeNode::Leaf { .. })
     }
 
     fn densify_weights(&mut self, max_sparse_density: f32) {
         fn densify(weights: &mut [Option<Vector>], max_sparse_density: f32) {
-            for w in weights.iter_mut() {
-                if let Some(w) = w {
-                    if !w.is_dense() && w.density() > max_sparse_density {
-                        w.densify();
-                    }
+            for w in weights.iter_mut().flatten() {
+                if !w.is_dense() && w.density() > max_sparse_density {
+                    w.densify();
                 }
             }
         }

src/model/train.rs
@@ -8,7 +8,6 @@ use itertools::{izip, Itertools};
 use log::info;
 use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
-use std::iter::FromIterator;
 use std::sync::{Arc, Mutex};
 use std::time;
 
@@ -366,7 +365,7 @@ impl TrainingExamples {
     }
 
     fn find_examples_with_labels(&self, labels: &[Index]) -> Vec<usize> {
-        let labels = IndexSet::from_iter(labels.iter().cloned());
+        let labels: IndexSet = labels.iter().cloned().collect();
         self.label_sets
             .par_iter()
             .enumerate()

commit e2b71000e3aa616e27e13087cfe2949e34b255d8

Fix cli args parsing
This was accidentally broken in commit 074dbfd95fab1f22dfd06c6710e11cdd79bdfb4d, which caused `--linear.*` and `--cluster.*` cli args to be ignored.

src/bin/omikuji.rs
@@ -24,8 +24,25 @@ fn parse_train_hyper_param(matches: &clap::ArgMatches) -> omikuji::model::TrainH
         collapse_every_n_layers: value_t!(matches, "collapse_every_n_layers", usize).unwrap(),
         tree_structure_only: matches.is_present("tree_structure_only"),
         train_trees_1_by_1: matches.is_present("train_trees_1_by_1"),
-        ..Default::default()
+        linear: omikuji::model::liblinear::HyperParam {
+            loss_type: match matches.value_of("linear.loss").unwrap() {
+                "hinge" => omikuji::model::liblinear::LossType::Hinge,
+                "log" => omikuji::model::liblinear::LossType::Log,
+                _ => unreachable!(),
+            },
+            eps: value_t!(matches, "linear.eps", f32).unwrap(),
+            c: value_t!(matches, "linear.c", f32).unwrap(),
+            weight_threshold: value_t!(matches, "linear.weight_threshold", f32).unwrap(),
+            max_iter: value_t!(matches, "linear.max_iter", u32).unwrap(),
+        },
+        cluster: omikuji::model::cluster::HyperParam {
+            k: value_t!(matches, "cluster.k", usize).unwrap(),
+            balanced: matches.occurrences_of("cluster.unbalanced") == 0,
+            eps: value_t!(matches, "cluster.eps", f32).unwrap(),
+            min_size: value_t!(matches, "cluster.min_size", usize).unwrap(),
+        },
     };
+
     hyper_param.validate().unwrap();
     hyper_param
 }