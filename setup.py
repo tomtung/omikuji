bef8c6f1e481d1ea7ffb61effce6fad245b5bb23
Yubing Dong (Tom) <tom.tung.dyb@gmail.com>
2022-02-01 01:54:14 -0800

    Drop support for Python 3.6
    Python 3.6 has reached its end-of-life date, and no longer supported by the latest version of pip, which is breaking our CI scripts.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     ),
     long_description=load_readme(),
     long_description_content_type="text/markdown",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     url="https://github.com/tomtung/omikuji",
     license="MIT",
     packages=["omikuji"],
@@ -50,8 +50,6 @@ setup(
     milksnake_universal=False,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
